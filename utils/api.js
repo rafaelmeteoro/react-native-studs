0502ba305e271fdab55ab8d370789ad9c360a8b7
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-03-22 23:51:10 -0300

    Add History component

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,10 @@
 import { AsyncStorage } from 'react-native'
-import { CALENDAR_STORAGE_KEY } from './_calendar'
+import { formatCalendarResults, CALENDAR_STORAGE_KEY } from './_calendar'
+
+export function fetchCalendarResults() {
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+        .then(formatCalendarResults)
+}
 
 export function submitEntry({ entry, key }) {
     return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
@@ -10,7 +15,7 @@ export function submitEntry({ entry, key }) {
 export function removeEntry(key) {
     return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
         .then((results) => {
-            const data = JSON.stringify(results)
+            const data = JSON.parse(results)
             data[key] = undefined
             delete data[key]
             AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
